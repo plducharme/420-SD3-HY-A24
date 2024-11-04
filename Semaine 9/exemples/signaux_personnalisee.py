ee6d358f8a5c12b712ff24fed4dadf5d0f5b9fb8
Pier Luc Ducharme <dutch80@gmail.com>
2024-11-04 18:24:45 -0500

    renommer signaux_personnalisee.py

--- Semaine 9/exemples/signaux_personnalisee.py
+++ Semaine 9/exemples/signaux_personnalisee.py
@@ -9,10 +9,11 @@ class Fenetre(QMainWindow):
         widget_central = QWidget()
         self.setCentralWidget(widget_central)
         disposition = QVBoxLayout()
+
         self.etiquette = QLabel("")
         self.mon_line_edit = MonQLineEdit()
         # Connecte le signal personalisé à une méthode
-        self.mon_line_edit.monSignal.connect(self.monSignal_declenche)
+        self.mon_line_edit.mon_signal.connect(self.mon_signal_declenche)
         bouton_test = QPushButton("Test")
         bouton_test.clicked.connect(self.bouton_clicked)
         disposition.addWidget(self.etiquette)
@@ -21,7 +22,7 @@ class Fenetre(QMainWindow):
         widget_central.setLayout(disposition)
 
     # Porter attention à la signature de la méthode
-    def monSignal_declenche(self, nouveau_texte: str, longueur_texte: int):
+    def mon_signal_declenche(self, nouveau_texte: str, longueur_texte: int):
         self.etiquette.setText(nouveau_texte + " " + str(longueur_texte))
 
     # Utiliser un bouton pour appeler le setText() du MonQLineEdit qui dclenchera le signal "monSignal"
@@ -31,7 +32,7 @@ class Fenetre(QMainWindow):
 # Un widget personnalisé qui va envoyer le signal personnalisé lorsque le setText() est appelé
 class MonQLineEdit(QLineEdit):
     # défini notre signal avec sa signature
-    monSignal = Signal(str, int)
+    mon_signal = Signal(str, int)
     def __init__(self):
         super().__init__()
 
@@ -39,7 +40,7 @@ class MonQLineEdit(QLineEdit):
     def setText(self, arg__1: str) -> None:
         super().setText(arg__1)
         # Émission du signal
-        self.monSignal.emit(arg__1, len(self.text()))
+        self.mon_signal.emit(arg__1, len(self.text()))
 
 
 app = QApplication()
